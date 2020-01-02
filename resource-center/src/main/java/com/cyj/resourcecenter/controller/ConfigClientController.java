25b626dd9b8c0c2184350be2a3cf7692770515de
chenxiban <chen867647213@163.com>
2020-01-02 15:11:23 +0800

    提交配置中心的修改，2020-1-2 15:11:17。

--- resource-center/src/main/java/com/cyj/resourcecenter/controller/ConfigClientController.java
+++ resource-center/src/main/java/com/cyj/resourcecenter/controller/ConfigClientController.java
@@ -1,9 +1,13 @@
 package com.cyj.resourcecenter.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @Description: 配置中心配置控制器
@@ -14,24 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @Email: chen87647213@163.com
  * @Version: 1.0
  */
+@Slf4j
+@Configuration
+@RefreshScope // 用于刷新配置
 @RestController
-@RequestMapping(value = "/config/client")
 public class ConfigClientController {
-    /** 外部属性 account */
-    @Value("${account}")
-    private String account;
-    /** 外部属性 repositoryUrl*/
-    @Value("${repositoryUrl}")
-    private String repositoryUrl;
 
-    @GetMapping("/getRepositoryUrl")
-    public String getRepositoryUrl() {
-        StringBuilder resultUrl = new StringBuilder("Account：");
-        resultUrl.append(account)
-                .append("<br/>")
-                .append("repositoryUrl：")
-                .append(repositoryUrl);
-        return resultUrl.toString();
+    @Resource
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        log.info("configInfo=================>"+configInfo);
+        return configInfo;
     }
+
 }
 
